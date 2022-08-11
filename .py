f962fb5ad9bbca4b48b45c47608e8c5b073cee2d
363/-/3/ <83331577+3G3M3N@users.noreply.github.com>
2022-08-11 15:06:13 +0300

    Update and rename .gitignore to .py

---
+++ .py
@@ -0,0 +1,37 @@
+import pandas as pd 
+
+df = pd.read_csv("qs-world-university-rankings-2017-to-2022-V2.csv")
+df
+
+turkey = df.loc[(df["country"]=="Turkey")]
+turkey
+
+turkey2017 = turkey.loc[(turkey["year"]==2017)]
+turkey2017
+turkey2017.shape
+turkey2017["city"].value_counts()
+
+turkey2018 = turkey.loc[(turkey["year"]==2018)]
+turkey2018
+turkey2018.shape
+turkey2018["city"].value_counts()
+
+turkey2019 = turkey.loc[(turkey["year"]==2019)]
+turkey2019
+turkey2019.shape
+turkey2019["city"].value_counts()
+
+turkey2020 = turkey.loc[(turkey["year"]==2020)]
+turkey2020
+turkey2020.shape
+turkey2020["city"].value_counts()
+
+turkey2021 = turkey.loc[(turkey["year"]==2021)]
+turkey2021
+turkey2021.shape
+turkey2021["city"].value_counts()
+
+turkey2022 = turkey.loc[(turkey["year"]==2022)]
+turkey2022
+turkey2022.shape
+turkey2022["city"].value_counts()
