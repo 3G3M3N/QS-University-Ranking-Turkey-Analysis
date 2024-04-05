f180d44552bb2a5509e804342d8d549e6d978c6a
3G3M3N <83331577+3G3M3N@users.noreply.github.com>
2024-04-05 18:28:00 +0300

    Update .py

--- .py
+++ .py
@@ -43,3 +43,25 @@ Turkey2023
 yeni_2024 = pd.read_csv("2024 QS World University Rankings 1.1 (For qs.com).csv")
 turkish2024 = yeni_2024.loc[df["Country"]=="Turkey"]
 turkish2024
+
+#matplotlib
+
+import matplotlib.pyplot as plt
+
+# Grafik boyutlarını ayarlama
+plt.figure(figsize=(15, 10))
+
+# Çubuk grafik oluşturma
+plt.barh(turkish2024['Institution Name'], turkish2024['2024 RANK'], color='blue', label='2024 Rank')
+plt.barh(turkish2024['Institution Name'], turkish2024['2023 RANK'], color='orange', label='2023 Rank')
+
+# Eksen etiketlerini ve başlığı ekleme
+plt.xlabel('Rank')
+plt.ylabel('University')
+plt.title('Comparison of Turkish Universities Ranks in 2023 and 2024')
+plt.legend()
+
+# Grafik gösterme
+plt.gca().invert_yaxis()
+plt.show()
+
